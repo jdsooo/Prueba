ded09a3563abe128dec7947b55dcb4526841e295
jdsooo <jdsaquiqu@gmail.com>
2024-05-30 11:28:33 -0600

    Diseñoheader

--- header/header.js
+++ header/header.js
@@ -1,3 +1,6 @@
 let header_DOM = document.querySelector(".header")
 
-header_DOM.innerHTML = `Hola soy hader`;    
+header_DOM.innerHTML = `
+    <h1 class="header-title">Gramática html</h1>
+    <button class="header-button">Menú</button>
+`;    
